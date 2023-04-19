1cd0abdf10fa385dfb59f85f01cba2d4106554c5
Intern User <intern1@globalland.com.ph>
2023-04-19 09:40:16 +0800

    For merging

--- resources/js/Components/Misc/Input/FloatingTextArea.vue
+++ resources/js/Components/Misc/Input/FloatingTextArea.vue
@@ -1,9 +1,8 @@
 <template>
 <div class="flex flex-col relative text-xs w-full  mb-5">
     <label for="Remarks" class="absolute mx-2 text-left text-[10px] px-1 uppercase -top-[7px] bg-white">{{ label }}</label>
-    <textarea rows="4" class="resize-none block p-2.5  text-[10px] text-gray-900 bg-white border border-gray-300" :class="[inputHeight,inputWidth]">
+    <textarea rows="4" class="resize-none block p-2.5  text-[10px] text-gray-900 bg-white border border-gray-300" :class="[inputHeight,inputWidth]"/>
 
-    </textarea>
 </div>
 </template>
 
--- resources/js/Components/Misc/Tabs/TabNav.vue
+++ resources/js/Components/Misc/Tabs/TabNav.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="relative">
-        <div class="flex whitespace-nowrap font-medium text-center text-gray-500 border-gray-200">
+        <div class="flex whitespace-nowrap font-medium text-center text-gray-500 ">
             <div v-for="tab in tabs" :key="tab" class="mr-1">
                 <router-link @click="setTab(tab)"
                              to="" href="#" aria-current="page"
                              :class="[tabLength,setHover, [selected === tab ? setSelectedBg : '']]"
-                             class="inline-block py-2 border border-gray-200 duration-300 my-1 uppercase
+                             class="inline-block py-2 border duration-300 my-1 uppercase
                               hover:text-white text-[11px] shadow-md tracking-widest font-rubik-light "
                              >
                             <!-- <div class=" text-lg">
@@ -43,11 +43,11 @@ export default {
         },
         setHover: {
             type: String,
-            default: 'hover:bg-[#F9951E] '
+            default: 'hover:bg-[#F9951E] hover:border-[#F9951E] '
         },
         setSelectedBg: {
             type: String,
-            default: 'bg-[#F9951E] text-white'
+            default: 'bg-[#F9951E] text-white border border-[#F9951E]'
         }
     },
     methods: {
