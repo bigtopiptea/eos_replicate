8bcd1af557bd201ebbd504c4aae1bf2c1d8330f6
Intern User <intern1@globalland.com.ph>
2023-04-26 08:11:27 +0800

    Done STR FILE NEW

---
+++ resources/js/Components/Admin/Trading/STR/STR.vue
@@ -0,0 +1,56 @@
+
+<script>
+import STRFileNew from "./STRFileNew.vue";
+import STRHistory from '@/Components/Admin/Trading/STR/STRHistory.vue';
+import Accordion from "@/Components/Misc/Accordion.vue";
+import SmallHeading from "@/Components/Misc/Heading/SmallHeading.vue";
+import TabNav from "@/Components/Misc/Tabs/TabNav.vue";
+import Tab from "@/Components/Misc/Tabs/Tab.vue";
+
+
+
+export default {
+    components: {
+        SmallHeading,
+        TabNav,
+        Tab,
+        STRFileNew,
+        STRHistory,
+
+
+
+    },
+    data(){
+        return {
+
+            selected: "File New",
+
+
+        }
+    },
+    methods: {
+        setSelected(tab) {
+            this.selected = tab;
+        },
+
+
+    },
+
+}
+
+</script>
+<template>
+    <div class="w-full h-full bg-white">
+        <div class="border mx-3 pt-3 bg-white border-white shadow-md">
+            <TabNav :tabs="['File New' , 'History']" :selected="selected" @selected="setSelected" :setBorder="'border-[#EE3E2C]'" :setHover="'hover:bg-[#EE3E2C]'" :setSelectedBg="'bg-[#EE3E2C] text-white border-[#EE3E2C]'">
+                <Tab :isSelected="selected === 'File New'" >
+                    <STRFileNew/>
+                </Tab>
+                <Tab :isSelected="selected === 'History'" >
+                    <STRHistory/>
+                </Tab>
+            </TabNav>
+        </div>
+    </div>
+</template>
+
---
+++ resources/js/Components/Admin/Trading/STR/STRFileNew.vue
@@ -0,0 +1,98 @@
+<script>
+import SearchIcon from '../../../Misc/Icons/SearchIcon.vue'
+import NormalButton from '../../../Misc/Buttons/NormalButton.vue'
+import InputGroup from '../../../Misc/Input/InputGroup.vue';
+import InputDropdown from '../../../Misc/Input/InputDropdown.vue';
+import FloatingLabelInput from '../../../Misc/Input/FloatingLabelInput.vue';
+import FloatingTextArea from '../../../Misc/Input/FloatingTextArea.vue';
+import SolidButton from '../../../Misc/Buttons/SolidButton.vue';
+export default{
+
+    components:{
+        SearchIcon,
+        NormalButton,
+        InputGroup,
+        InputDropdown,
+        FloatingLabelInput,
+        FloatingTextArea,
+        SolidButton,
+
+
+
+    },
+
+    data(){
+        return{
+
+            myOptions:['1','2','3']
+        }
+    }
+}
+</script>
+<template>
+    <div class="py-3 px-6">
+        <!-- 1st Content -->
+        <div>
+            <form class="flex items-start">
+                <div class="relative ">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <SearchIcon />
+                    </div>
+                    <input type="text" id="simple-search"
+                        class="bg-gray-50 h-[34px] border border-r-0 border-[#EAEAEA] text-gray-900 font-light text-[10px] block w-full pl-10 p-2.5"
+                        placeholder="Search" required />
+                </div>
+                <NormalButton label="Go"
+                class="p-1.5 px-3 uppercase h-[34px] bg-[#F9951E] text-[10px]  text-white" />
+            </form>
+        </div>
+        <!-- 2nd Content -->
+        <div class="w-full flex gap-5 mt-5">
+            <div class="w-1/2 ">
+                <div>
+                    <InputGroup :inputLabel="'REFERENCE NO.'" :inputWidth="'w-8/12'" :labelWidth="'w-4/12'"/>
+                </div>
+                <div>
+                    <InputGroup :inputLabel="'CUSTOMER NAME'" :inputWidth="'w-8/12'" :labelWidth="'w-4/12'"/>
+                </div>
+                <div>
+                    <InputDropdown :inputLabel="'CUSTOMER NAME'" :inputWidth="'w-8/12'" :labelWidth="'w-4/12'" :choices="myOptions"/>
+                </div>
+            </div>
+
+            <div class="w-1/2 ">
+                <div>
+                    <InputDropdown :inputLabel="'TRANSACTION TYPE'" :inputWidth="'w-8/12'" :labelWidth="'w-4/12'" :choices="myOptions"/>
+                </div>
+                <div>
+                    <InputGroup :inputLabel="'ACCOUNT NAME'" :inputWidth="'w-8/12'" :labelWidth="'w-4/12'"/>
+                </div>
+                <div class="flex gap-3">
+                    <InputGroup :inputLabel="'Volume'" :inputWidth="'w-7/12'" :labelWidth="'w-5/12'"/>
+                    <InputGroup :inputLabel="'Rate'" :inputWidth="'w-7/12'" :labelWidth="'w-5/12'"/>
+                    <InputGroup :inputLabel="'Mode'" :inputWidth="'w-7/12'" :labelWidth="'w-5/12'"/>
+                </div>
+            </div>
+        </div>
+        <!-- 3rd Content -->
+        <div class="flex flex-col">
+            <div class="w-full flex gap-3 ">
+                <InputGroup :inputLabel="'AMOUNT'" :inputWidth="'w-6/12'" :labelWidth="'w-6/12'"/>
+                <InputDropdown :inputLabel="'TYPE'" :inputWidth="'w-6/12'" :labelWidth="'w-6/12'" :choices="myOptions"/>
+                <InputGroup :inputLabel="'REGISTRATION DATE'" :inputWidth="'w-6/12'" :labelWidth="'w-6/12'" :inputType="'date'"/>
+                <InputGroup :inputLabel="'DEAL SLIP NO.'" :inputWidth="'w-6/12'" :labelWidth="'w-6/12'"/>
+            </div>
+            <FloatingLabelInput :inputLabel="'ADDRESS'" :inputWidth="'w-full'" :inputColor="'bg-white'" />
+            <div class="w-full my-3">
+                <InputGroup :inputLabel="'TYPE OF SUSPICIOUS ACTIVITY'" :inputWidth="'w-full'" />
+            </div>
+            <div>
+                <FloatingTextArea :label="'DESCRIPTION'" :inputHeight="'h-[100px]'"/>
+            </div>
+            <div class="flex justify-center gap-5">
+                <SolidButton :buttonLabel="'Cancel'"/>
+                <SolidButton :buttonLabel="'Submit'" :buttonStyle="'bg-#F9951E'"/>
+            </div>
+        </div>
+    </div>
+</template>
