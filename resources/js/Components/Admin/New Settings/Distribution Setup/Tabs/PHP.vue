efce04b3f47f9c8592ba2db95cf4d56553d185b5
Intern User <intern1@globalland.com.ph>
2023-05-08 09:06:20 +0800

    Started Distribution Setup

---
+++ resources/js/Components/Admin/New Settings/Distribution Setup/Tabs/PHP.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    
+}
+</script>
+<template>
+    <div class="h-auto w-full bg-white">
+        <div class="py-3 px-5 ">
+            PHP
+        </div>
+    </div>
+</template>
