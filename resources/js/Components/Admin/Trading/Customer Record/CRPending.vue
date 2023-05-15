7aeb8321ba8587af09f5ea6af29401d63f721db3
quintansvedasto01 <quintansvedasto01@gmail.com>
2023-05-15 15:38:13 +0800

    for merge

--- resources/js/Components/Admin/Trading/Customer Record/CRPending.vue
+++ resources/js/Components/Admin/Trading/Customer Record/CRPending.vue
@@ -268,22 +268,22 @@ export default {
         <div class="flex flex-col justify-between mx-5 mb-5 mt-[20px] h-full">
             <div class="flex justify-between">
                 <router-link to="/app/trading/customer-record/individual">
-                    <div class="indiv-client flex flex-col justify-center items-center ">
-                        <svg class="h-[176px] w-[196px] border-[3px] border-[#F9951E] hover:bg-[#F9951E]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#F9951E">
+                    <div class="flex flex-col justify-center items-center ">
+                        <p class="text-[#F9951E] text-center text-[14px] font-bold border-[3px] border-[#F9951E] hover:bg-[#F9951E] hover:text-white">
+                            <svg class="h-[150px] w-[200px] hover:stroke-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#F9951E">
                             <path stroke-linecap="round" stroke-linejoin="round"
                             d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                         </svg>
-                        <p class="text-[#F9951E] text-center text-[14px] font-bold">
-                            INDIVIDUAL CLIENT
+                        INDIVIDUAL CLIENT
                         </p>
                     </div>
                 </router-link>
                 <router-link to="/app/trading/customer-record/corporate">
-                    <div class="corp-client flex flex-col justify-center items-center">
-                        <svg class="h-[176px] w-[196px] border-[3px] border-[#F9951E] hover:bg-[#F9951E]" fill="none" stroke="#F9951E" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"></path>
-                        </svg>
-                        <p class="text-[#F9951E] text-center text-[14px] font-bold">
+                    <div class="flex flex-col justify-center items-center ">
+                        <p class="text-[#F9951E] text-center text-[14px] font-bold border-[3px] border-[#F9951E] hover:bg-[#F9951E] hover:text-white">
+                            <svg class="h-[150px] w-[200px] hover:stroke-white"  fill="none" stroke="#F9951E" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"></path>
+                            </svg>
                             CORPORATE CLIENT
                         </p>
                     </div>
@@ -295,9 +295,9 @@ export default {
         </div>
     </ModalTwo>
 </template>
-<style scoped>
+<!-- <style scoped>
 .indiv-client :hover,
 .corp-client :hover{
     stroke: white;
 }
-</style>
+</style> -->
