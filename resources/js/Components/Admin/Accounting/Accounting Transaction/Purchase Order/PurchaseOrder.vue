08bafbedcef0a4fdbe66e847987356466b17e46e
Intern User <intern1@globalland.com.ph>
2023-07-05 08:05:36 +0800

    Hello

--- resources/js/Components/Admin/Accounting/Accounting Transaction/Purchase Order/PurchaseOrder.vue
+++ resources/js/Components/Admin/Accounting/Accounting Transaction/Purchase Order/PurchaseOrder.vue
@@ -22,8 +22,8 @@
             <div class="flex justify-center">
                 <h1 class="uppercase text-[12px] font-[500] text-black">Purchase Order</h1>
             </div>
-            <div class="flex w-full gap-3">
-                <div class="flex flex-col border w-[33%] gap-5">
+            <div class="flex w-full gap-3 mb-5">
+                <div class="flex flex-col w-[33%] justify-between">
                     <div>
                         <InputGroupSelectMenu :label="'Supplier'" :inputWidth="'w-6/12'" :labelWidth="'w-6/12'" :options="sampleOption"/>
                     </div>
@@ -37,11 +37,18 @@
                         <InputGroup :inputLabel="'Contact No.'" :isDisabled="true" :inputWidth="'w-6/12'" :labelWidth="'w-6/12'" />
                     </div>
                 </div>
-                <div class="flex flex-col border w-[33%]">
-                    hello
-                    hello
+                <div class="flex flex-col w-[33%] gap-3">
+                    <div>
+                        <TextAreaGroup :inputLabel="'Supplier Address'" :labelWidth="'w-6/12'" :inputWidth="'w-6/12'"/>
+                    </div>
+                    <div>
+                        <InputGroupSelectMenu :label="'Deliver to'" :inputWidth="'w-6/12'" :labelWidth="'w-6/12'" :options="sampleOption"/>
+                    </div>
+                    <div>
+                        <TextAreaGroup :inputLabel="'Delivery Address'" :labelWidth="'w-6/12'" :inputWidth="'w-6/12'"/>
+                    </div>
                 </div>
-                <div class="flex flex-col border w-[33%] gap-3">
+                <div class="flex flex-col w-[33%] justify-between">
                     <div>
                         <InputGroup :inputLabel="'PO DATE'" :isDisabled="true" :inputWidth="'w-6/12'" :labelWidth="'w-6/12'" :inputType="'date'" />
                     </div>
@@ -57,7 +64,54 @@
                     <div>
                         <InputGroup :inputLabel="'Contact No.'" :isDisabled="true" :inputWidth="'w-6/12'" :labelWidth="'w-6/12'" />
                     </div>
-
+                </div>
+            </div>
+            <div class="min-w-full py-2 align-middle ">
+                <div class="relative h-[360px]">
+                    <div class="shadow ring-1 ring-black ring-opacity-5 overflow-auto absolute inset-x-0 min-h-auto max-h-full">
+                        <table class="min-w-full divide-y divide-gray-300">
+                            <thead class="bg-[#EDEDED] font-medium text-[11px] whitespace-nowrap sticky top-0">
+                                <tr class="divide-x divide-gray-200">
+                                    <th v-for="label in labels" :key="label.label" scope="col"
+                                        class="py-1 px-2 uppercase tracking-wider text-center whitespace-nowrap text-gray-900 ">
+                                    {{ label.label }}
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="divide-y divide-gray-200 bg-white font-light text-[10px]">
+                                <tr class="divide-x divide-gray-200">
+                                    <td
+                                        class="whitespace-nowrap text-center uppercase py-1 px-2 tracking-wider">
+                                        01
+                                    </td>
+                                    <td
+                                        class="whitespace-nowrap text-center uppercase py-1 px-2 tracking-wider">
+                                        <CheckboxSelectMenu :placeholder="'ALLCASH SJDM BULACAN'"/>
+                                    </td>
+                                    <td
+                                        class="whitespace-nowrap text-center uppercase py-1 px-2 tracking-wider">
+                                        <CheckboxSelectMenu :placeholder="'INPUT/select item (PRODUCT/SERVICE)'"/>
+                                    </td>
+                                    <td
+                                        class="whitespace-nowrap text-center uppercase py-1 px-2 tracking-wider">
+                                        <CheckboxSelectMenu :placeholder="'select unit'"/>
+                                    </td>
+                                    <td
+                                        class="whitespace-nowrap text-center uppercase py-1 px-2 tracking-wider">
+                                        <input type="number" name="" id="quantity" class="w-full py-1.5 px-2 uppercase text-[10px] border border-gray-300 focus:outline-none">
+                                    </td>
+                                    <td
+                                        class="whitespace-nowrap text-center uppercase py-1 px-2 tracking-wider">
+                                        php
+                                    </td>
+                                    <td
+                                        class="whitespace-nowrap text-center uppercase py-1 px-2 tracking-wider">
+                                        10,000.00
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
@@ -72,13 +126,15 @@ import BorderButton from '@/Components/Misc/Buttons/BorderButton.vue'
 import ModalTwo from '../../../../Misc/Modal/ModalTwo.vue'
 import InputGroup from '../../../../Misc/Input/InputGroup.vue'
 import InputGroupSelectMenu from '../../../../Misc/Select Menu/InputGroupSelectMenu.vue'
+import TextAreaGroup from '../../../../Misc/Input/TextAreaGroup.vue'
+import CheckboxSelectMenu from '../../../../Misc/Select Menu/CheckboxSelectMenu.vue'
 
 
     export default {
 
         components:{
             TabNav, Tab,Pending, ApprovalHistory,BorderButton,ModalTwo,InputGroup
-            ,InputGroupSelectMenu
+            ,InputGroupSelectMenu, TextAreaGroup, CheckboxSelectMenu
         },
         data(){
 
@@ -86,7 +142,20 @@ import InputGroupSelectMenu from '../../../../Misc/Select Menu/InputGroupSelectM
 
                 selected:'Pending',
                 openModal:false,
-
+                sampleOption:[
+                    {label:'A'},
+                    {label:'B'},
+                    {label:'C'},
+                ],
+                labels:[
+                    {label:'#'},
+                    {label:'Cost Center'},
+                    {label:'Description'},
+                    {label:'Unit'},
+                    {label:'Quantity'},
+                    {label:'Unit Price'},
+                    {label:'Amount'},
+                ]
             }
         },
 
