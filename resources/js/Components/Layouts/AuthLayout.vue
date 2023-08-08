8130c14371791fefc5a2b409667d7a8c5e68b0bc
quintansvedasto01 <quintansvedasto01@gmail.com>
2023-08-08 15:06:23 +0800

    asd

--- resources/js/Components/Layouts/AuthLayout.vue
+++ resources/js/Components/Layouts/AuthLayout.vue
@@ -823,7 +823,7 @@ export default {
         <!-- Swap to ml-64 if you dont want a sticky sidebar -->
         <div class="flex flex-col flex-grow ml-64" :class="!isToggled ? 'ml-32' : ''">
             <!-- Navbar -->
-            <div class="fixed bg-white mt-[0.15rem] right-0 left-64 z-10 px-4 py-2 " :class="!isToggled ? 'left-32' : ''">
+            <div class="fixed bg-white mt-[0.15rem] right-0 left-64 z-10 px-4 py-[1px] " :class="!isToggled ? 'left-32' : ''">
                 <div class="min-w-0 flex-1 flex justify-between ">
                     <div v-if="!isToggled">
                         <h1 class="uppercase text-[11px]">{{ user.first_name }} {{user.last_name}}</h1>
@@ -869,7 +869,7 @@ export default {
                 </ul>
             </div>
 
-            <div class="flex-grow bg-[#ECECEC] min-h-screen  px-4 py-6 my-10 mt-6">
+            <div class="flex-grow bg-[#ECECEC] min-h-screen  p-4 my-10 mt-6">
                 <Title class="font-bold mt-4" :title="this.$route.meta.title" />
                 <router-view></router-view>
             </div>
--- resources/js/Components/Misc/Section/Title.vue
+++ resources/js/Components/Misc/Section/Title.vue
@@ -8,8 +8,8 @@ const props = defineProps({
 </script>
 <template>
     <div class="w-full">
-        <div class="w-full h-10 bg-gradient-to-r from-[#FB9E30] via-[#F98B38] to-[#EE3E2C]">
-            <h1 class="float-right py-2.5  px-4 text-[14px] font-rubik-light tracking-wide text-white uppercase">
+        <div class="w-full h-[36px] bg-gradient-to-r from-[#FB9E30] via-[#F98B38] to-[#EE3E2C]">
+            <h1 class="float-right py-2.5  px-4 text-[12px] font-rubik-light tracking-wide text-white uppercase">
                 {{ props.title }}
             </h1>
         </div>
